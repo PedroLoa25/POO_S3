a8d07d623f4d4f0b855bc2fee43e8c07ad5b98e7
PedroLoa25 <pfloayza1704@gmail.com>
2025-08-29 14:02:46 -0500

    polimorfismo added

---
+++ src/main/java/pe/edu/upeu/polimorfismo/Loro.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.polimorfismo;
+
+public class Loro extends Animal{
+    @Override
+    public void emitirSonido(){
+        System.out.println("Hola, como estas... despierta!");
+    }
+}
